9d6c1aa129d01817596b84ca121e254bf781078c
Daniele Gazzelloni <dangz.dev@gmail.com>
2025-10-15 11:49:50 +0100

    chore: basic error management in movie list

--- src/components/AddMovieForm.vue
+++ src/components/AddMovieForm.vue
@@ -1,24 +1,48 @@
 <template>
-  <form class="flex flex-col mt-10 w-[400px]">
+  <form class="flex flex-col mt-10 w-[400px]" @submit.prevent="addMovie">
     <label for="title">Movie title:</label>
-    <input id="title" v-model="movie.title" />
+    <input id="title" v-model="movie.title" :class="{ '!border-red-600': isError }" @input="isError = false" />
 
     <label for="director">Director:</label>
     <input id="director" v-model="movie.director" />
 
     <label for="year">Year:</label>
     <input id="year" v-model="movie.year" type="number" />
+
+    <button
+      class="mt-4 bg-blue-500 text-white py-2 px-4 rounded hover:bg-blue-600 transition"
+      type="submit"
+    >
+      Add Movie
+    </button>
   </form>
 </template>
 
 <script setup lang="ts">
-import { reactive } from 'vue';
+import { ref, reactive } from 'vue';
 import type { Movie } from '@/stores/movies.type';
+
+const isError = ref<boolean>(false);
 
 const movie = reactive<Movie>({
   uuid: crypto.randomUUID(),
   title: '',
 });
+
+function validateMovie(m: Movie): boolean {
+  return !!m.title;
+}
+
+function addMovie() {
+  if (!validateMovie(movie)) {
+    isError.value = true;
+    return;
+  }
+
+  // Logic to add the movie
+  console.log('Adding movie:', movie);
+}
+
 </script>
 
 <style scoped>
