ef24775cb8f9c3e19a3ac7902b99c60099a8e5c4
Daniele Gazzelloni <dangz.dev@gmail.com>
2025-10-15 12:23:41 +0100

    feat: add remove move feature

--- src/components/MoviesList.vue
+++ src/components/MoviesList.vue
@@ -3,9 +3,19 @@
     <h2 class="text-2xl font-bold mb-4">Movies List</h2>
     <ul class="space-y-2">
       <li v-for="movie in movies" :key="movie.uuid" class="p-4 border rounded shadow-sm hover:shadow-md transition">
-        <h3 class="text-xl font-semibold">{{ movie.title }}</h3>
-        <p class="text-gray-600">Director: {{ movie.director }}</p>
-        <p class="text-gray-600">Year: {{ movie.year }}</p>
+        <h3 class="text-xl font-semibold">
+          {{ movie.title }}
+        </h3>
+
+        <p class="text-gray-600">
+          Director: {{ movie.director }}
+        </p>
+
+        <p class="text-gray-600">
+          Year: {{ movie.year }}
+        </p>
+
+        <RemoveMovieBtn :uuid="movie.uuid" />
       </li>
     </ul>
   </div>
@@ -14,6 +24,8 @@
 <script setup lang="ts">
 import { storeToRefs } from 'pinia';
 import { useMoviesStore } from '@/stores/moviesStore';
+
+import RemoveMovieBtn from './RemoveMovieBtn.vue';
 
 const moviesStore = useMoviesStore();
 const { movies } = storeToRefs(moviesStore);
---
+++ src/components/RemoveMovieBtn.vue
@@ -0,0 +1,20 @@
+<template>
+  <button
+    class="bg-red-500 text-white py-1 px-2 rounded hover:bg-red-600 transition text-xs mt-4"
+    @click="removeMovie(uuid)"
+  >
+    Remove
+  </button>
+</template>
+
+<script setup lang="ts">
+import type { Movie } from '@/stores/movies.type';
+import { defineProps } from 'vue';
+import { useMoviesStore } from '@/stores/moviesStore'; 
+
+defineProps<{
+  uuid: Movie['uuid'];
+}>();
+
+const { removeMovie } = useMoviesStore();
+</script>
